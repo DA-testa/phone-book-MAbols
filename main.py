85c4291314407b02db5eb466c25740e9cdcf0336
Mārcis Ābols <116550290+MAbols@users.noreply.github.com>
2023-03-30 17:15:53 +0000

    edit 1

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ class Query:
         if self.type == 'add':
             self.name = query[2]
 
+def hash_func(number):
+    ans=(2*number+3)%4
+    return ans%5
+
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
@@ -17,28 +21,34 @@ def write_responses(result):
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    contacts = {}
     for cur_query in queries:
+        number=cur_query.number
         if cur_query.type == 'add':
             # if we already have contact with such number,
             # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            hashed=hash_func(number)
+            
+            if hashed in contacts.keys():
+           
+                contacts[hashed][number]=cur_query.name
+
+            else:
+                contacts[hashed]={}
+                contacts[hashed][number]=cur_query.name
+            
+            
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            hashed=hash_func(number)
+            if hashed in contacts.keys():
+                del(contacts[hashed][number])
+        
         else:
             response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            hashed=hash_func(number)
+            if hashed in contacts.keys():
+                if number in contacts[hashed].keys():
+                    response=contacts[hashed][number]
             result.append(response)
     return result
 
